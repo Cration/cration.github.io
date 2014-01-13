a1e01bbabd5e938a6214053af3ba238545b0c1d5
Cration <leichen_523@qq.com>
2014-01-13 11:13:16 +0800

    delete some useless files.

--- _plugins/excerpt_filter.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-    module ExcerptFilter
-        def extract_excerpt(input)
-            input.split('<!--excerpt-->')[1]
-        end
-    end
-end
-
-Liquid::Template.register_filter(Jekyll::ExcerptFilter)
